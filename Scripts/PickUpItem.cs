d7c5fa5cc4cd57023127de2aaf119d445eb38ff7
Halil İbrahim Koçak <98169696+HalilKocak06@users.noreply.github.com>
2025-12-12 22:07:24 +0300

    V2

---
+++ Scripts/PickUpItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    public enum ItemType { ImpactWrench}
+    public ItemType itemType = ItemType.ImpactWrench;
+    
+}
